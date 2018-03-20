de2487cecb6c210aa17d332996add5d1f3832c20
Jeppe Andersen <jeppe@outlook.com>
2018-03-20 18:23:03 +0100

    Add github link to footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -13,7 +13,7 @@ export default class Footer extends React.PureComponent {
           Note for Firefox users, if you have privacy.resistFingerprinting enabled, latency resolution is lowered.
         </p>
         <p>
-          Created by <a href="https://twitter.com/nocture">@nocture</a> (<a href="http://www.nocture.dk">nocture.dk</a>). Fork on GitHub.
+          Created by <a href="https://twitter.com/nocture">@nocture</a> (<a href="http://www.nocture.dk">nocture.dk</a>). <a href="https://github.com/jlandersen/aws-speed-test">Fork</a> on GitHub.
         </p>
         <p>
           Based on <a href="https://azurespeedtest.azurewebsites.net">Azure Speed Test 2.0</a>.
